c7c446f7a5d481a35a43c22a5b5c390a545ec61c
Caleb Simmons <97637707+iHoopcs@users.noreply.github.com>
2023-04-05 01:34:34 -0400

    Cart Container Item Styling

--- my-app/src/Components/Cart/DisplayCartItem.js
+++ my-app/src/Components/Cart/DisplayCartItem.js
@@ -45,7 +45,7 @@ export default function DisplayCartItem(props){
                 <div className='row just'>
                     <div className='col-9'>
                         <div className='d-flex'>
-                            <div className='card mb-4 mx-4 mt-4'>
+                            <div className='card mb-4 mx-5 mt-4'>
                                 <img src={item.imageUrl} alt={item.name} />
                             </div>
 
