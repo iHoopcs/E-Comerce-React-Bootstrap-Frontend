a3b9cff2702684966f79eef31809681d4576f220
Caleb Simmons <97637707+iHoopcs@users.noreply.github.com>
2023-04-05 04:06:49 -0400

    Login & Signup Minor Changes
    Implement handleSubmit function -> navigate to homepage on login

--- my-app/src/Components/LoginSignup/Signup.js
+++ my-app/src/Components/LoginSignup/Signup.js
@@ -1,31 +1,32 @@
 import {Form} from "react-bootstrap";
 import {Button} from "react-bootstrap";
 import Header from "../Header";
+
 export default function Signup(){
     return (
         <>
             <Header />
             <div className='container form-container '>
                 <h2 className='form-title'>Please fill out information</h2>
-                <Form>
+                <Form action='http://localhost:8080/addUser' method='post'>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
                         <Form.Label>First Name</Form.Label>
-                        <Form.Control type="fName" placeholder="First Name" />
+                        <Form.Control type="text" placeholder="First Name" name='firstName' />
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="formBasicPassword">
                         <Form.Label>Last Name</Form.Label>
-                        <Form.Control type="lName" placeholder="Last Name" />
+                        <Form.Control type="text" placeholder="Last Name" name='lastName' />
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="formBasicPassword">
                         <Form.Label>Account Username</Form.Label>
-                        <Form.Control type="username" placeholder="Account Username" />
+                        <Form.Control type="text" placeholder="Account Username" name='username' />
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="formBasicPassword">
                         <Form.Label>Account Password</Form.Label>
-                        <Form.Control type="password" placeholder="Account Password" />
+                        <Form.Control type="password" placeholder="Account Password" name='password' />
                     </Form.Group>
 
                     <Button variant="primary" type="submit">
